3b13dd32cebdd650bc856b98587a28536c5789e2
Ammar Faizi <ammarfaizi2@gmail.com>
2017-05-14 10:44:00 +0700

    Fix class

--- index.php
+++ index.php
@@ -8,11 +8,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', true);
 
-try{
-	new aaaaa;
-} catch(\Exception $e){
-	print "error nih";
-}
+
 require __DIR__ . '/vendor/autoload.php';
 use System\Crayner;
 use System\Config_Handler;
